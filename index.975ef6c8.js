3fd4d4fdaf6446b3c80c799904d25cc70bdacc8e
Joel Kizito <106533534+kizitojoel@users.noreply.github.com>
2024-01-11 11:01:47 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27172,7 +27172,7 @@ var _reactDefault = parcelHelpers.interopDefault(_react);
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -27189,7 +27189,7 @@ const imageAltText = "Grey pattern";
  */ const skillsList = [
     "Web design",
     "User experience",
-    "Machine Learning",
+    "Machine learning",
     "Inclusive design",
     "Focus group testing",
     "Mobile user interfaces",
@@ -28617,7 +28617,7 @@ var _propTypesDefault = parcelHelpers.interopDefault(_propTypes);
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
